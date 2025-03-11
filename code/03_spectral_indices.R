710b92dee11d4f9d640cc9998b0d1afe0260bb71
Federico Pesare <federico.pesare@studio.unibo.it>
2025-03-11 17:43:10 +0100

    Create 03_spectral_indices.R

---
+++ code/03_spectral_indices.R
@@ -0,0 +1,46 @@
+# Code to calculate spectral indices from satellite images
+
+library(terra)
+library(imageRy)
+library(viridis)
+
+#quali immagini ho a disposizione?
+im.list()
+
+# importare l'immagine satellitare del matogrosso del 1992
+mato1992 = im.import("matogrosso_l5_1992219_lrg.jpg")
+# se serve invertire l'immagine usare la f flip
+mato1992 = flip(mato1992) 
+
+# 1 = NIR
+# 2 = red
+# 3 = green
+
+im.plotRGB(mato1992, r=1, g=2, b=3)
+# oppure 
+plot(mato1992)
+
+# NIR su green
+im.plotRGB(mato1992, r=2, g=1, b=3)
+# NIR su blue
+im.plotRGB(mato1992, r=2, g=3, b=1)
+
+# importare l'immagine satellitare del matogrosso del 2006
+mato2006 = im.import("matogrosso_ast_2006209_lrg.jpg")
+mato2006 = flip(mato2006)
+plot(mato2006)
+
+# visualizzare le due immagini (1992 e 2006) affiancate
+im.multiframe(1,2)
+im.plotRGB(mato1992, r=2, g=3, b=1)
+im.plotRGB(mato2006, r=2, g=3, b=1)
+
+# Radiometric resolution
+plot(mato1992[[1]], col=inferno(100))
+plot(mato2006[[1]], col=inferno(100))
+
+# Range da 0 a 250 indica immagini a 8 bit
+
+# Difference Vegetation Index (DVI), informazioni sullo stato di salute delle piante
+# Plant: NIR=255, red=0, DVI=NIR-red = 255 
+# Stressed plant: NIR=100, red=20, DVI=100-20 = 80
