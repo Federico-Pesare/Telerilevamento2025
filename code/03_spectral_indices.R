03f078a6460ac520caa2b3d24c530888156d6c91
Federico Pesare <federico.pesare@studio.unibo.it>
2025-03-13 15:47:21 +0100

    Update 03_spectral_indices.R

--- code/03_spectral_indices.R
+++ code/03_spectral_indices.R
@@ -44,3 +44,70 @@ plot(mato2006[[1]], col=inferno(100))
 # Difference Vegetation Index (DVI), informazioni sullo stato di salute delle piante
 # Plant: NIR=255, red=0, DVI=NIR-red = 255 
 # Stressed plant: NIR=100, red=20, DVI=100-20 = 80
+
+# Calculating DVI
+# 1 = NIR
+# 2 = red
+
+# NIR - red
+dvi1992 = mato1992[[1]] - mato1992[[2]] 
+plot(dvi1992)
+
+# range DVI (da -255 a 255)
+# maximum: NIR - red = 255 - 0 = 255
+# minimum: NIR - red = 0 - 255 = -255
+
+#con un'altra variazione di colore (aver scaricato viridis permette di evitare l'impiego del comando colorramppalette
+plot(dvi1992, col=magma(100))
+
+# DVI per mato2006
+dvi2006 = mato2006[[1]] - mato2006[[2]]
+plot(dvi2006)
+
+# uno accanto all'altro
+im.multiframe(1,2)
+plot(dvi1992)
+plot(dvi2006)
+
+# Different radiometric resolution, quando abbiamo risoluzioni diverse occorre normalizzare, es 8 e 4 bit
+
+# DVI 8 bit: range (0-255)
+# maximum: NIR - red = 255 - 0 = 255
+# minimum: NIR - red = 0 - 255 = -255
+
+# DVI 4 bit: range (0-15)
+# maximum: NIR - red = 15 - 0 = 15
+# minimum: NIR - red = 0 - 15 = -15
+
+# NORMALIZZATO (rapporto)
+
+# NDVI 8 bit: range (0-255)
+# maximum: (NIR - red) / (NIR + red) = (255 - 0) / (255 + 0) = 1
+# minimum: (NIR - red) / (NIR + red) = (0 - 255) / (0 + 255) = -1
+
+# NDVI 4 bit: range (0-15)
+# maximum: (NIR - red) / (NIR + red) = (15 - 0) / (15 + 0) = 1
+# minimum: (NIR - red) / (NIR + red) = (0 - 15) / (0 + 15) = -1
+
+ndvi1992 = (mato1992[[1]] - mato1992[[2]]) / (mato1992[[1]] + mato1992[[2]])
+plot(ndvi1992)
+
+ndvi2006 = (mato2006[[1]] - mato2006[[2]]) / (mato2006[[1]] + mato2006[[2]])
+plot(ndvi2006)
+
+# oppure tramite una f di imageRy
+dvi1992auto = im.dvi(mato1992,1,2)
+plot(dvi1992auto)
+
+dvi2006auto = im.dvi(mato2006,1,2)
+plot(dvi2006auto)
+
+ndvi1992auto = im.ndvi(mato1992,1,2)
+plot(ndvi1992auto)
+
+ndvi2006auto = im.ndvi(mato2006,1,2)
+plot(ndvi2006auto)
+
+im.multiframe(1,2)
+
+
