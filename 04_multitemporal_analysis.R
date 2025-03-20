64b94a4de1bec4717897b2736173312966f95461
Federico Pesare <federico.pesare@studio.unibo.it>
2025-03-20 14:26:08 +0100

    Update 04_multitemporal_analysis.R

--- 04_multitemporal_analysis.R
+++ 04_multitemporal_analysis.R
@@ -39,29 +39,6 @@ dev.off()
 grdif = gr[[1]] - gr[[4]]
 plot(grdif)
 
-# EXPORTING DATA
-
-#1 choose the folder (directory)
-setwd()
-# C:\Users\feder\Documents ,  remember to change backslash on Windows!!!
-setwd("C:/Users/feder/Documents")
-
-getwd()
-plot(gr)
-
-# create the file (png, pdf, jpeg, ...)
-png("greenland_output.png")
-plot(gr)
-dev.off()
-
-pdf("greendiff.pdf")
-plot(grdif)
-dev.off()
-
-jpeg("greendiff.jpeg)
-plot(grdif)
-dev.off()
-
 
 
 
