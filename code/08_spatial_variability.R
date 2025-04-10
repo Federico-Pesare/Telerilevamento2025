bf0947e19dbb1c2a7d4c12d1fa280a6b739d59ca
Federico Pesare <federico.pesare@studio.unibo.it>
2025-04-10 15:33:29 +0200

    Update 08_spatial_variability.R

--- code/08_spatial_variability.R
+++ code/08_spatial_variability.R
@@ -61,6 +61,65 @@ p3 = ggRGB(sent, r=1, g=2, b=3)
 p1 + p2 + p3
 
 
+# NEW: What to do in case of huge images
+sent = im.import("sentinel.png")
+sent = flip(sent)
+plot(sent)
+
+# calculating the number of pixel
+ncell(sent)                     #633612
+ncell(sent) * nlyr(sent)        #2534448 final value of pixel number
+
+senta = aggregate(sent, fact=2)
+ncell(senta) * nlyr(senta)         #633612
+
+senta5 = aggregate(sent, fact=5)
+ncell(senta5) * nlyr(senta5)        #101760
+
+# make a multiframe and plot in RGB the 3 images (or, 2, 5)
+im.multiframe(1,3)
+im.plotRGB(sent, 1, 2, 3)                  # we can use directly 1,2,3 instead of r=1, g=2, b=3
+im.plotRGB(senta,1, 2, 3)
+im.plotRGB(senta5, 1, 2, 3)
+# HIGH RESOLUTION IS NOT NECESSARY EVERY TIME ! A LOWER ONE COULD BE BETTER IN SOME CASES
+
+
+# calculating standard deviation
+nir = sent[[1]]
+sd3 = focal(nir, w=c(3,3), fun="sd")
+
+nira = senta[[1]]
+sd3a = focal(nira, w=c(3,3), fun="sd")
+
+nira5 = senta5[[1]]
+sd3a5 = focal(nira5, w=c(3,3), fun="sd")
+
+nir5a5 = sent5a5[[1]]
+sd5a5 = focal(nir5a5, w=c(5,5), fun="sd")
+
+im.multiframe(2,2)
+plot(sd3)
+plot(sd3a)
+plot(sd3a5)
+plot(sd5a5)
+
+#or
+p1 = im.ggplot(sd3)
+p2 = im.ggplot(sd3a)
+p3 = im.ggplot(sd3a5)
+p4 = im.ggplot(sd5a5)
+p1+p2+p3+p4
+
+# Variance
+var3 = sd3^2
+
+im.multiframe(1,2)
+plot(sd3)
+plot(var3)
+# si perde tutta la variabilità intermedia, le mappe di varianza non sono molto impiegate come la deviazione standard (di fatti sul pacchetto terra la funzione focal non è implementata per la varianza).
+# può essere ok per mostrare il confronto fra zone ad altissima variabilità e a bassissima variabilità
+
+
 ##############################################
 # Code to measure spatial varibaility considering both (i) spectral distances and (ii) relative abundances
 
