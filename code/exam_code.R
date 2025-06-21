f3fd5861fdefb53ba5d4e7f188723eb6fc58fc09
Federico Pesare <federico.pesare@studio.unibo.it>
2025-06-21 19:25:36 +0200

    Update exam_code.R

--- code/exam_code.R
+++ code/exam_code.R
@@ -5,6 +5,9 @@ library
 
 # land-cover classification
 # importo e classifico 2017 dry
-path = "C:/Users/feder/Desktop/Tsiribihina/2017DRY/JPG/set2017.jpg"
-set2017 = rast(path)
-set2017c = im.classify(set2017, num_clusters=3)
+> path = "C:/Users/feder/Desktop/Tsiribihina/2017DRY/JPG/2017dry.jpg"
+> dry2017 = rast(path)
+> plot(dry2017)
+> dry2017=flip(dry2017)
+> plot(dry2017)
+> dry2017c = im.classify(dry2017, num_clusters=3)
