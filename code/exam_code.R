155cfc5051072e8739a5c4b6b532472ac0c7e984
Federico Pesare <federico.pesare@studio.unibo.it>
2025-06-22 00:47:34 +0200

    Update exam_code.R

--- code/exam_code.R
+++ code/exam_code.R
@@ -3,29 +3,25 @@ library(terra)
 library(imageRy)
 library
 
-# land-cover classification
-# importo e classifico 2017 dry
+# 1. LAND-COVER CLASSIFICATION
+
+# 2017 DRY
 > path="C:/Users/feder/Desktop/Tsiribihina/2017DRY/JPG/dry2017.jpg"
 > dry2017=rast(path) 
 > plot(dry2017)
 > dry2017=flip(dry2017)
 > plot(dry2017)
 
-# creo maschera per eliminare i pixel neri
-mask <- dry2017[[1]] == 0 & dry2017[[2]] == 0 & dry2017[[3]] == 0
-# applicazione della maschera, nero = NA
-dry2017_masked <- mask(dry2017, mask, maskvalue=1)
-# plot, stretch="lin" ricalibra i pixel sulla scala RGB aumentando i contrasti
-plotRGB(dry2017_masked, r=1, g=2, b=3, stretch="lin")
-#classificazione
-dry2017c=im.classify(dry2017_masked, num_clusters=3)
-# class 1 = foresta decidua
-# class 2 = suolo degradato
+> dry2017c = im.classify(dry2017, num_clusters=3)
 
 # calcolo area percentuale
 pdry2017 = freq(dry2017c)*100 / ncell(dry2017c)
 pdry2017
-# class1 = 45.64
-# class2 = 31.57
-
+# class1 = vegetazione = 45.62%
+# class2 = acqua = 16.84%
+# class3 = suolo nudo = 37.54%
+# composizione percentuale proporzionata alla terra emersa (100% - 16.84% = 83.16%)
+# vegetazione:  x : 100 = 45.62 : 83.16  ->  54.86%
+# suolo nudo:  x : 100 = 37.54 : 83.16  ->  45.14%
+ 
 
