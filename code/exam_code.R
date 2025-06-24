275ce48aa3f4f71cb4a98f36c93b99d843dfacda
Federico Pesare <federico.pesare@studio.unibo.it>
2025-06-25 01:51:10 +0200

    Update exam_code.R

--- code/exam_code.R
+++ code/exam_code.R
@@ -250,9 +250,9 @@ nbr <- rast(list.files(pattern = "NBR\\d{2}\\.tif$"))
 # 3. Rinomina i layer con gli anni corretti
 names(nbr) <- c("2019", "2020", "2021", "2022", "2023", "2024", "2025")
  
-# Ridgeline plot
+# Ridgeline plot, forzare lo sfondo bianco per la palette usata (rocket)
 Rnbr = im.ridgeline(ndvi, scale=1, palette="rocket")
-Rnbr = Rnbr + labs(x = "NBR", y = "anno") + theme_minimal()
+Rnbr = Rnbr + labs(x = "NBR", y = "anno") + theme_minimal()  + theme(panel.background = element_rect(fill = "white", colour = NA),plot.background = element_rect(fill = "white", colour = NA))
 
 # salvataggio
 ggsave("nbr_ridgeline.png", Rnbr, width = 8, height = 5, dpi = 300)
